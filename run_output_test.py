e6d1aa2b0cc9b312c2ea1e6e089aab3f15ef8a97
Dan Bryan <dbryan@civisanalytics.com>
2020-11-19 13:21:04 -0600

    Test run output posting

--- run_output_test.py
+++ run_output_test.py
@@ -6,4 +6,4 @@ script_id = os.environ.get("CIVIS_JOB_ID")
 run_id = os.environ.get("CIVIS_RUN_ID")
 file_id = 134753755
 
-client.post_containers_runs_outputs(script_id, run_id, "File", file_id)
+client.scripts.post_containers_runs_outputs(script_id, run_id, "File", file_id)
